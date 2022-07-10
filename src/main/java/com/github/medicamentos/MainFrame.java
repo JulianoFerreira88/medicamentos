3bbe9053edf4424741674142fafd698bb2374d98
JulianoFerreira88 <julianoferreira336@gmail.com>
2022-07-10 17:52:12 -0300

    atualizando projeto

--- src/main/java/com/github/medicamentos/MainFrame.java
+++ src/main/java/com/github/medicamentos/MainFrame.java
@@ -1,6 +1,5 @@
 package com.github.medicamentos;
 
-import java.awt.Color;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -29,62 +28,48 @@ public class MainFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel6 = new javax.swing.JPanel();
         jPanel5 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         txtProduto = new javax.swing.JTextField();
-        jSeparator1 = new javax.swing.JSeparator();
         jScrollPane1 = new javax.swing.JScrollPane();
         jList1 = new javax.swing.JList<>();
         painelChart = new javax.swing.JPanel();
         barChart1 = new com.github.medicamentos.components.BarChart();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        getContentPane().setLayout(new java.awt.CardLayout());
+        setUndecorated(true);
+        setPreferredSize(new java.awt.Dimension(750, 600));
 
-        jPanel6.setBackground(new java.awt.Color(153, 153, 254));
-        jPanel6.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
+        jPanel5.setBackground(java.awt.Color.darkGray);
 
-        jPanel5.setBackground(new java.awt.Color(153, 153, 254));
-
-        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBackground(java.awt.Color.darkGray);
         jPanel1.setPreferredSize(new java.awt.Dimension(400, 200));
 
+        jPanel3.setBackground(java.awt.Color.darkGray);
         jPanel3.setOpaque(false);
         jPanel3.setPreferredSize(new java.awt.Dimension(400, 40));
         jPanel3.setLayout(new javax.swing.BoxLayout(jPanel3, javax.swing.BoxLayout.PAGE_AXIS));
 
-        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel2.setBackground(java.awt.Color.darkGray);
         jPanel2.setPreferredSize(new java.awt.Dimension(522, 25));
+        jPanel2.setLayout(new java.awt.CardLayout());
 
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("Pesquisar Produto");
         jLabel1.setAlignmentY(0.0F);
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 522, Short.MAX_VALUE)
-            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 522, Short.MAX_VALUE))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 36, Short.MAX_VALUE)
-            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel1)
-                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-        );
+        jPanel2.add(jLabel1, "card2");
 
         jPanel3.add(jPanel2);
 
+        txtProduto.setBackground(java.awt.Color.darkGray);
         txtProduto.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
-        txtProduto.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        txtProduto.setForeground(new java.awt.Color(0, 255, 204));
+        txtProduto.setBorder(null);
+        txtProduto.setCaretColor(new java.awt.Color(255, 255, 255));
+        txtProduto.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
         txtProduto.setMargin(new java.awt.Insets(2, 15, 2, 2));
         txtProduto.setPreferredSize(new java.awt.Dimension(2, 30));
         txtProduto.addActionListener(new java.awt.event.ActionListener() {
@@ -99,17 +84,20 @@ public class MainFrame extends javax.swing.JFrame {
         });
         jPanel3.add(txtProduto);
 
-        jSeparator1.setPreferredSize(new java.awt.Dimension(50, 15));
-        jPanel3.add(jSeparator1);
-
+        jScrollPane1.setBackground(java.awt.Color.darkGray);
+        jScrollPane1.setBorder(null);
         jScrollPane1.setPreferredSize(new java.awt.Dimension(7, 250));
 
+        jList1.setBackground(java.awt.Color.darkGray);
+        jList1.setForeground(new java.awt.Color(0, 255, 204));
         jList1.setModel(new javax.swing.AbstractListModel<String>() {
             String[] strings = { " " };
             public int getSize() { return strings.length; }
             public String getElementAt(int i) { return strings[i]; }
         });
         jList1.setPreferredSize(new java.awt.Dimension(5, 250));
+        jList1.setSelectionBackground(new java.awt.Color(255, 255, 255));
+        jList1.setSelectionForeground(new java.awt.Color(0, 0, 0));
         jList1.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
             public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
                 jList1ValueChanged(evt);
@@ -125,7 +113,7 @@ public class MainFrame extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, 522, Short.MAX_VALUE)
+                .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, 546, Short.MAX_VALUE)
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -138,21 +126,22 @@ public class MainFrame extends javax.swing.JFrame {
         painelChart.setBackground(new java.awt.Color(255, 255, 255));
         painelChart.setLayout(new java.awt.CardLayout());
 
-        barChart1.setChartColor(java.awt.Color.white);
-        barChart1.setChartLabelsColor(java.awt.Color.black);
+        barChart1.setChartColor(java.awt.Color.darkGray);
+        barChart1.setChartLabelsColor(java.awt.Color.white);
         barChart1.setChartLabelsVisible(true);
-        barChart1.setChartMarksColor(java.awt.Color.black);
+        barChart1.setChartMarksColor(java.awt.Color.white);
         barChart1.setChartMarksVisible(true);
-        barChart1.setChartSeriesColor(new java.awt.Color(0, 0, 51));
-        barChart1.setChartTitle("<Not Set>");
-        barChart1.setChartTitleColor(java.awt.Color.black);
+        barChart1.setChartSeriesColor(new java.awt.Color(0, 255, 204));
+        barChart1.setChartTitle("");
+        barChart1.setChartTitleColor(java.awt.Color.white);
+        barChart1.setOpaque(false);
         painelChart.add(barChart1, "card2");
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
         jPanel5Layout.setHorizontalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 542, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 566, Short.MAX_VALUE)
             .addComponent(painelChart, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
         );
         jPanel5Layout.setVerticalGroup(
@@ -160,27 +149,19 @@ public class MainFrame extends javax.swing.JFrame {
             .addGroup(jPanel5Layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 171, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(painelChart, javax.swing.GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE))
+                .addComponent(painelChart, javax.swing.GroupLayout.DEFAULT_SIZE, 273, Short.MAX_VALUE))
         );
 
-        javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
-        jPanel6.setLayout(jPanel6Layout);
-        jPanel6Layout.setHorizontalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel6Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel5, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-        jPanel6Layout.setVerticalGroup(
-            jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel6Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jPanel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
-
-        getContentPane().add(jPanel6, "card3");
 
         pack();
         setLocationRelativeTo(null);
@@ -207,9 +188,7 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel5;
-    private javax.swing.JPanel jPanel6;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JPanel painelChart;
     private javax.swing.JTextField txtProduto;
     // End of variables declaration//GEN-END:variables
@@ -240,7 +219,7 @@ public class MainFrame extends javax.swing.JFrame {
             JDBCCategoryDataset dataSet = new JDBCCategoryDataset(con, query);
             barChart1.setData(dataSet);
             barChart1.setChartTitle(Arrays.toString(list.toArray()));
-            barChart1.setChartTitleColor(Color.BLACK);
+
         } catch (Exception e) {
         }
     }
